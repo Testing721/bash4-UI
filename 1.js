c082db89623a2955c593beae2ab45d30390f33c6
Testing721 <musaibk2022@gmail.com>
2023-02-03 13:02:26 +0500

    commit1
    1.1

---
+++ 1.js
@@ -0,0 +1,14 @@
+let a = "Musaib"
+let b = "khan"
+console.log(a , b);
+
+// function abc () {
+//     console.log("this is branch1");
+// }
+
+// abc()
+
+// function xyz () {
+//     console.log("this is branch2");
+// }
+ 
