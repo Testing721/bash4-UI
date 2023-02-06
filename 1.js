0b9472c19983fd39fe71ba0e42337f054ae55d2e
Testing721 <musaibk2022@gmail.com>
2023-02-06 15:05:52 +0500

    Revert "revert9"
    This reverts commit 9210c36a507369464dc804cec07af5998d709233.

---
+++ 1.js
@@ -0,0 +1,22 @@
+let a = "Musaib"
+let b = "khan"
+console.log(a , b);
+
+console.log("Helooooooooooooo");
+
+// function abc () {
+//     console.log("this is branch1");
+// }
+
+// abc()
+
+// function xyz () {
+//     console.log("this is branch2");
+// }
+ 
+
+
+
+console.log("qwerty");
+
+console.log("hiiiiiiiiiiiiiiiiii");
